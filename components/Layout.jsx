ed65c1e344e5f856586d50b83e46e1a41ccca8c9
shruti c <starlight.shruti@gmail.com>
2022-12-23 02:10:58 +1100

    initial code

---
+++ components/Layout.jsx
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const Layout = ({Children}) => {
+  return (
+    <div>
+        <Navbar/>
+        {Children}
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default Layout

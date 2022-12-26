e3c77b6e318ac4ad40347d8a0a98f3e355821d28
shruti c <starlight.shruti@gmail.com>
2022-12-26 18:19:52 +1100

    Slider code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,15 @@
-import Featured from '../components/Featured';
-import Footer from '../components/Footer';
-import Layout from '../components/Layout'
-import Navbar from '../components/Navbar';
-import '../styles/globals.css'
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
   return (
-  // <Layout >
     <>
-        <Navbar/>
-        
-    <Component {...pageProps} /> 
-        <Footer/>
+      <Navbar />
+        <Component {...pageProps} />
+      <Footer />
     </>
-  )
-  
+  );
 }
 
 export default App;
